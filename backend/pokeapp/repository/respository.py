085e8da8c3b5753b5b52bd7c5c9e67dd92593179
filpss <filipepinho2010@hotmail.com>
2024-08-31 15:20:29 -0300

    Adicionando métodos para hash da senha que vai ser persistida no banco de dados

--- backend/pokeapp/repository/respository.py
+++ backend/pokeapp/repository/respository.py
@@ -2,10 +2,11 @@ from sqlalchemy.orm import Session
 from ..models.models import User
 from sqlalchemy.exc import IntegrityError
 from fastapi import HTTPException
+from backend.pokeapp.security import get_password_hash
 
 def register_user(database: Session, username: str, password: str):
     try:
-        db_user = User(username=username, password=password)
+        db_user = User(username=username, password=get_password_hash(password))
         database.add(db_user)
         database.commit()
         database.refresh(db_user)
